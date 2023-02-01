4551d0da71baa9102e54f86599f2c03dc057dbe5
Sergey Drachev <serzh272@mail.ru>
2023-02-01 22:49:02 +0900

    ExamEntity fixed.

--- app/src/main/java/ru/serzh272/nfp/data/local/database/entity/ExamEntity.kt
+++ app/src/main/java/ru/serzh272/nfp/data/local/database/entity/ExamEntity.kt
@@ -1,18 +1,23 @@
 package ru.serzh272.nfp.data.local.database.entity
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.Index
-import androidx.room.PrimaryKey
+import androidx.room.*
 import java.util.*
 
 @Entity(
     tableName = "exam",
-    indices = [Index("id", unique = true)]
+    indices = [
+        Index("id", unique = true),
+        Index("user_id", unique = false),
+    ],
+    foreignKeys = [
+        ForeignKey(UserEntity::class, parentColumns = ["id"], childColumns = ["user_id"], onUpdate = ForeignKey.CASCADE, onDelete = ForeignKey.CASCADE)
+    ]
 )
 data class ExamEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
+    @ColumnInfo(name = "user_id")
+    val userId: Long,
     @ColumnInfo(name = "exam_date")
     val examDate: Date,
     val description: String? = null
