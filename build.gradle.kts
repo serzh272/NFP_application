fdeb301d153b7112912c57c3ffcbc42b87ba5fb8
Sergey Drachev <serzh272@mail.ru>
2023-08-06 21:48:09 +0900

    Parcelize plugin removed.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,4 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.dagger.hilt.android) apply false
     alias(libs.plugins.kotlin.kapt) apply false
-    alias(libs.plugins.kotlin.parcelize) apply false
 }
