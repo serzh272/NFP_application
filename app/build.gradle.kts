3c4e8001c9b8a578458db37a37382b862cfd885b
Sergey Drachev <serzh272@mail.ru>
2023-08-06 18:59:14 +0900

    Version catalogs added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id ("com.android.application")
-    id ("org.jetbrains.kotlin.android")
-    id ("kotlin-kapt")
-    id ("com.google.dagger.hilt.android")
-    id ("kotlin-parcelize")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
--- app/src/main/java/ru/serzh272/nfp/data/local/database/dao/PointsDao.kt
+++ app/src/main/java/ru/serzh272/nfp/data/local/database/dao/PointsDao.kt
@@ -8,6 +8,11 @@ import ru.serzh272.nfp.data.local.database.entity.enums.WeightConstraint
 @Dao
 interface PointsDao {
 
-    @Query("SELECT points.points FROM points LEFT JOIN weight_constraint ON points.id = weight_constraint.points_id WHERE exercise_id = :exerciseId AND gender = :gender AND result = :result AND `constraint` = :weightConstraint")
+    @Query("SELECT points.points FROM points " +
+        "LEFT JOIN weight_constraint ON points.id = weight_constraint.points_id " +
+        "WHERE exercise_id = :exerciseId AND " +
+        "gender = :gender AND " +
+        "result = :result AND " +
+        "`constraint` = :weightConstraint")
     suspend fun getPoints(exerciseId: Int, gender: Gender, result: Int, weightConstraint: WeightConstraint? = null): Int
 }
