d47572c6c42183b817c6844906a12a2a751dd481
Sergey Drachev <serzh272@mail.ru>
2024-02-29 15:17:30 +0900

    Скорректировано сохранение состояния экрана упражнений.

--- feature/norms/ui/src/main/java/ru/serzh272/nfp/norms/delegate/NormsFilterDelegate.kt
+++ feature/norms/ui/src/main/java/ru/serzh272/nfp/norms/delegate/NormsFilterDelegate.kt
@@ -1,13 +1,10 @@
 package ru.serzh272.nfp.norms.delegate
 
-import androidx.lifecycle.SavedStateHandle
 import ru.serzh272.nfp.norms.NormsViewModel
-import ru.serzh272.nfp.norms.model.ExerciseUi
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-class NormsFilterDelegate : ReadWriteProperty<NormsViewModel, Set<Long>> {
-
+fun selectedIds() = object : ReadWriteProperty<NormsViewModel, Set<Long>> {
     override fun getValue(thisRef: NormsViewModel, property: KProperty<*>): Set<Long> {
         return thisRef.savedStateHandle[property.name] ?: setOf()
     }
@@ -16,5 +13,3 @@ class NormsFilterDelegate : ReadWriteProperty<NormsViewModel, Set<Long>> {
         thisRef.savedStateHandle[property.name] = value
     }
 }
-
-fun selectedIds() = NormsFilterDelegate()
